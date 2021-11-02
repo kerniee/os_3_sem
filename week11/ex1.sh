5712d8b2e82df361b9f97737301c3438a1014b96
Kernie <39545943+kerniee@users.noreply.github.com>
2021-11-02 19:26:17 +0300

    Update ex1.sh

--- week11/ex1.sh
+++ week11/ex1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-device=$(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1)
+device_old=$(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1)
 
 umount ./lofsdisk
 [ -e lofs.img ] && rm lofs.img
-[ ! -z "$device" ] && losetup --detach $device
+[ ! -z "$device_old" ] && losetup --detach $device_old
 
 sudo fallocate -l 50M lofs.img
 sudo losetup -f lofs.img
