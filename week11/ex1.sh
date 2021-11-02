8579d2d5887fc155c9ba3ceddd76510282c3fbc9
Kernie <39545943+kerniee@users.noreply.github.com>
2021-11-02 19:24:33 +0300

    Fix dirty bug

--- week11/ex1.sh
+++ week11/ex1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-device=$(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1)
 umount ./lofsdisk
 [ -e lofs.img ] && rm lofs.img
 [ ! -z "$device" ] && losetup --detach $device
 
 sudo fallocate -l 50M lofs.img
 sudo losetup -f lofs.img
+
+device=$(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1)
+
 sudo mkfs.ext4 $device
 [ -d lofsdisk ] || mkdir lofsdisk
 sudo mount $device ./lofsdisk
